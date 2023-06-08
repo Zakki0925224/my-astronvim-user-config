2c5e6190b3de19a7176a0e1668dda03d2a15b584
Zakki <zakki0925224@gmail.com>
2023-06-08 13:08:35 +0900

    [Add]: Added mason list

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,24 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "bash",
+        "clangd",
+        "omnisharp",
+        "cssls",
+        "eslint",
+        "html",
+        "jsonls",
+        "jdtls",
+        "lua_ls",
+        "marksman",
+        "rust_analyzer",
+        "rustfmt",
+        "tsserver",
+        "lemminx",
+        "yamlls",
+        "yamlfix",
+        "yamlfmt",
+        "yamllint"
       })
     end,
   },
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,17 +1,17 @@
 return {
   {
     "TimUntersberger/neogit",
-  version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  event = "VeryLazy",
-  dependencies = {
-    "sindrets/diffview.nvim",
-  },
-  config = function()
-    require("neogit").setup {
-      integrations = {
-        diffview = true,
-      },
-    }
-  end,
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
   }
 }
